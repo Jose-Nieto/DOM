94f8b75b31c37a93f68f40def820755bf54b71b8
Jose-Nieto <joseemariaariveraa@gmail.com>
2020-09-13 19:07:27 -0500

    Agregando guia 8

--- guia8/js/eventosteclas.js
+++ guia8/js/eventosteclas.js
@@ -2,11 +2,19 @@ document.onkeydown = keyHit;
 var thisPic = 0;
 function keyHit(evt){
  var myPix = new Array(
- "img/leon.jpg",
-"img/tigre.jpg",
-"img/puma.jpg",
-"img/leopardo.jpg",
-"img/lince.jpg"
+"img/cara.jpg",
+ "img/finbalor.jpg",
+"img/johncena.jpg",
+"img/brock.jpg",
+"img/randyorton.jpg",
+"img/undertaker.jpg",
+
+"img/seth.jpg",
+"img/reymisterio.jpg",
+"img/kane.jpg",
+
+
+"img/kofi.jpg"
 );
  var imgCt = myPix.length - 1;
  //37 y 39 son los códigos de las teclas que representan
@@ -14,6 +22,8 @@ function keyHit(evt){
  //respectivamente
  var ltArrow = 37;
  var rtArrow = 39;
+ var up=38;
+ var down=40;
  //Manejo del objeto para controlar los eventos del teclado
  //de forma uniforme sin importar el navegador
  var thisKey = (evt) ? evt.which : window.event.keyCode;
@@ -22,6 +32,12 @@ function keyHit(evt){
  }
  else if(thisKey == rtArrow){
  chgSlide(1);
+ }
+ else if(thisKey==up){
+     chgSlide(1);
+ }
+ else if(thisKey==down){
+     chgSlide(-1);
  }
  return false;
 
